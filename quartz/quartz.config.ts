1c5d9e54c868fc02935f0f6fe1381fdcae642aa6
sycstudio <1726016246@qq.com>
2026-06-03 13:15:41 +0800

    try to fix the link error

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -68,7 +68,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
